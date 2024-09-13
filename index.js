1117c7762f6f97a489821549fcd60aec9d7043ea
Leo <135759885+ap-leo@users.noreply.github.com>
2024-09-13 04:44:48 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-var first = document.getElementById("first")
-var trd = document.getElementById("trd")
-var frt = document.getElementById("frt")
-const fft = document.getElementById('fft');
-
-var second = document.getElementById("second")
-var botaoSobre = document.getElementById("sobre")
-var botaoProjetos = document.getElementById("projetos")
-var botaoContato = document.getElementById("contato")
-var voltarSobre = document.getElementById("voltarSobre")
-var voltarProjetos = document.getElementById("voltarProjetos")
-var voltarProjeto = document.getElementById("voltarProjeto")
-var voltarContato = document.getElementById("voltarContato")
-var voltar = document.getElementById("inicio")
-
 
 var insta = document.getElementById("projeto1")
 var pitch3 = document.getElementById("projeto2")
