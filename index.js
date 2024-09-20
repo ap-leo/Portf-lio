05b234fb8d27de5bf1b95fdbcd46c3a0413fb06c
Leo <135759885+ap-leo@users.noreply.github.com>
2024-09-20 03:26:45 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 var insta = document.getElementById("projeto1")
 var inicio = document.getElementById("inicio")
-var trd = document.getElementById("row")
+var trd = document.getElementById("linha")
 var pitch3 = document.getElementById("projeto2")
 var pitch4 = document.getElementById("projeto3")
 var labmeta = document.getElementById("projeto4")
@@ -14,44 +14,56 @@ var cards = document.querySelectorAll(".card");
 var titulo = document.getElementById("titulo")
 var sub = document.getElementById("sub")
 var acesse = document.getElementById("acesse")
+var fft = document.getElementById("fft");
+var coluna = document.getElementById("cards");
+
+
 
 
 cards.forEach((card) => {
     card.addEventListener("click", () => {
+
         trd.style.height = "100vh"
         trd.style.right = "100vw"
+        coluna.style.display = "none"
 
         const classes = Array.from(card.classList);
 
         switch (classes[1]) {
             case "card1":
+                fft.style.background = " var(--azule, linear-gradient(179deg, #3DBFC3 29.73%, rgba(106, 172, 174, 0.81) 89.4%))"
                 titulo.innerText = "InSTAUFPR"
-                sub.innerText = "oi"
+                sub.innerHTML = "<p class = 'descricao'>Atualmente trabalho no Insituto de Soluções Aplicadas da UFPR. Sou social media, mas sempre ajudo com site dos professores e eventos que todos do setor de tecnologia da UFPR querem criar. Fiz muitos amigos e conexões aqui, além de ter dado um jeito no site deles!</p>"
                 acesse.innerHTML = "<a href='google.com'>Acesse</a>"
                 break;
             case "card2":
+                fft.style.background = " linear-gradient(260deg, #5D009B -72.57%, #350059 -24.61%, #8C00EB 96.8%)"
                 titulo.innerText = "oi"
-                sub.innerText = "oi"
+                sub.innerHTML = "<p class = 'descricao'>Atualmente trabalho no Insituto de Soluções Aplicadas da UFPR. Sou social media, mas sempre ajudo com site dos professores e eventos que todos do setor de tecnologia da UFPR querem criar. Fiz muitos amigos e conexões aqui, além de ter dado um jeito no site deles!</p>"
                 acesse.innerHTML = "<a href='google.com'>Acesse</a>"
                 break;
             case "card3":
+                fft.style.background = "black";
                 titulo.innerText = "oi"
-                sub.innerText = "oi"
+                sub.innerHTML = "<p class = 'descricao'>Atualmente trabalho no Insituto de Soluções Aplicadas da UFPR. Sou social media, mas sempre ajudo com site dos professores e eventos que todos do setor de tecnologia da UFPR querem criar. Fiz muitos amigos e conexões aqui, além de ter dado um jeito no site deles!</p>"
                 acesse.innerHTML = "<a href='google.com'>Acesse</a>"
                 break;
             case "card4":
+                fft.style.background = "linear-gradient(225deg, #F6971D 0.58%, #3DBFC3 74.43%)"
                 titulo.innerText = "oi"
-                sub.innerText = "oi"
+                sub.innerHTML = "<p class = 'descricao'>Atualmente trabalho no Insituto de Soluções Aplicadas da UFPR. Sou social media, mas sempre ajudo com site dos professores e eventos que todos do setor de tecnologia da UFPR querem criar. Fiz muitos amigos e conexões aqui, além de ter dado um jeito no site deles!</p>"
                 acesse.innerHTML = "<a href='google.com'>Acesse</a>"
                 break;
             case "card5":
+                fft.style.background = "var(--roxo, linear-gradient(180deg, #AF61C3 27.83%, #352339 114.35%))"
                 titulo.innerText = "oi"
-                sub.innerText = "oi"
+                sub.innerHTML = "<p class = 'descricao'>Atualmente trabalho no Insituto de Soluções Aplicadas da UFPR. Sou social media, mas sempre ajudo com site dos professores e eventos que todos do setor de tecnologia da UFPR querem criar. Fiz muitos amigos e conexões aqui, além de ter dado um jeito no site deles!</p>"
                 acesse.innerHTML = "<a href='google.com'>Acesse</a>"
                 break;
             case "card6":
+                fft.style.background = "linear-gradient(158deg, #218CEE 14.41%, #E7F5FF 114.33%)"
                 titulo.innerText = "oi"
-                sub.innerText = "oi"
+                sub.innerHTML = "<p class = 'descricao'>Atualmente trabalho no Insituto de Soluções Aplicadas da UFPR. Sou social media, mas sempre ajudo com site dos professores e eventos que todos do setor de tecnologia da UFPR querem criar. Fiz muitos amigos e conexões aqui, além de ter dado um jeito no site deles!</p>"
                 acesse.innerHTML = "<a href='google.com'>Acesse</a>"
                 break;
             default:
@@ -63,8 +75,10 @@ cards.forEach((card) => {
 })
 
 inicio.addEventListener("click", () => {
-    trd.style.height = "200vh"
+    trd.style.height = "auto"
     trd.style.right = "0vw"
+    coluna.style.display = "grid"
+
 })
 
 
@@ -72,10 +86,10 @@ window.addEventListener('scroll', () => {
     const distanciaParaMudar = window.innerHeight * 2.5;
 
     if (window.scrollY < distanciaParaMudar && window.scrollY < window.innerHeight * 4) {
-        texto.innerHTML = "Oi! Sou estudante de <strong>Ciência da Computação na UFPR</strong> e comecei a me aventurar no design e na programação em 2022, quando entrei no curso e no projeto de extenção <strong>Ciência para Todos (CPT)</strong>.";
+        texto.innerHTML = "Olá! Sou estudante de <strong>Ciência da Computação na UFPR</strong>. Comecei a me aventurar no <strong>design e programação</strong> em 2022, quando entrei no curso e no projeto de extenção Ciência para Todos (CPT).";
     } else if (window.scrollY < window.innerHeight * 3.5) {
-        texto.innerHTML = "Sou um <strong>Libriano de 22 anos</strong> que ama embelezar projetos e passar tempo com amigos";
+        texto.innerHTML = "Sou um <strong>libriano de 22 anos</strong> que ama organizar e dar um 'tchan' em projetos, mas também ama passar tempo com amigos";
     } else {
-        texto.innerHTML = "A maioria dos meus projetos foram voltados a eventos entre a <strong>comunidade interna e externa à UFPR</strong>, como você vai ver a seguir :)";
+        texto.innerHTML = "A maioria dos meus projetos foram voltados a eventos entre a <strong>comunidade interna e externa da UFPR</strong>, como você vai ver a seguir :)";
     }
 });
